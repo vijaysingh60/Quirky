0ca68b8d8cbb96a1169868b31f0d385c704ff095
vijaysingh60 <121249393+vijaysingh60@users.noreply.github.com>
2025-02-21 18:50:35 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ require("dotenv").config();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:3000', 
+    origin: 'http://quirky-tau.vercel.app', 
     credentials: true,
 }));
 //app.use(express.urlencoded{});
